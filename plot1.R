9bceb369cca90367caed303330d92592d99ba661
Younghoon Seo <80616847+YoungSeo1118@users.noreply.github.com>
2021-04-16 09:53:41 +0900

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,11 +1,14 @@
+#Download data
 setwd("/Users/macintosh/Downloads/exdata_data_NEI_data")
 emissions_data <- readRDS("summarySCC_PM25.rds")
 code_data <- readRDS("Source_Classification_Code.rds")
 library(dplyr)
 
+#Add the annual total emission
 emissions_annual<- emissions_data %>% group_by(year) %>% 
   summarize(totalEmissions=sum(Emissions,na.rm=T))
 
+#Plot
 dev.copy(png,'plot1.png')
 with(emissions_annual,plot(x=year,y=totalEmissions,
                            xlab="Year",
